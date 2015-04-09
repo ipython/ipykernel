b2a254ef2e703f3433cbb80bc967af5b4d8f88ce
Min RK <benjaminrk@gmail.com>
2015-04-09 10:45:40 -0700

    remove filter-branch residue

--- IPython/kernel/connect.py
+++
@@ -1,2 +0,0 @@
-from ipython_kernel.connect import *
-from jupyter_client.connect import *
--- IPython/utils/pickleutil.py
+++
@@ -1,5 +0,0 @@
-from warnings import warn
-
-warn("IPython.utils.pickleutil has moved to ipython_kernel.pickleutil")
-
-from ipython_kernel.pickleutil import *

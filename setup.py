256cd3f6ac9fe35d6b438b5c497581121ee1b80f
Min RK <benjaminrk@gmail.com>
2015-08-04 12:14:29 -0700

    depend on IPython 4.0 final

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ if 'develop' in sys.argv or any(a.startswith('bdist') for a in sys.argv):
 
 setuptools_args = {}
 install_requires = setuptools_args['install_requires'] = [
-    'ipython>=4.0.0.dev0',
+    'ipython>=4.0.0',
     'traitlets',
     'jupyter_client',
 ]

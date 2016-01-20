039c05705c23b2fe692d5ccd2d745a43435d2023
Jason Grout <jgrout6@bloomberg.net>
2016-01-20 12:24:53 -0500

    Update traitlets dependency version

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ if 'develop' in sys.argv or any(a.startswith('bdist') for a in sys.argv):
 setuptools_args = {}
 install_requires = setuptools_args['install_requires'] = [
     'ipython>=4.0.0',
-    'traitlets',
+    'traitlets>=4.1.0',
     'jupyter_client',
     'tornado>=4.0',
 ]

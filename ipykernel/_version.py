ae9ebd0da9cff1b9a5fd6e409c0de44892da9100
Steven Silvester <steven.silvester@ieee.org>
2019-08-07 15:41:01 -0500

    switch to release version

--- ipykernel/_version.py
+++ ipykernel/_version.py
@@ -1,4 +1,4 @@
-version_info = (5, 2, 0, 'dev')
+version_info = (5,1,2)
 __version__ = '.'.join(map(str, version_info[:3]))
 
 # pep440 is annoying, beta/alpha/rc should _not_ have dots or pip/setuptools

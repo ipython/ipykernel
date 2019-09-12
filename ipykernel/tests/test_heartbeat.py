d4d870c3229ef29116f7b37985de9229be86767f
Mark Dickinson <mdickinson@enthought.com>
2019-09-12 15:29:00 +0100

    Fix Heartbeat._bind_socket to return on the first successful bind.

--- ipykernel/tests/test_heartbeat.py
+++ ipykernel/tests/test_heartbeat.py
@@ -21,6 +21,14 @@ def test_port_bind_failure_raises():
         assert mock_try_bind.call_count == 1
 
 
+def test_port_bind_failure_succeeds():
+    heart = Heartbeat(None)
+    with patch.object(heart, '_try_bind_socket') as mock_try_bind:
+        mock_try_bind.side_effect = lambda: None
+        heart._bind_socket()
+        assert mock_try_bind.call_count == 1
+
+
 def test_port_bind_failure_recovery():
     try:
         errno.WSAEADDRINUSE

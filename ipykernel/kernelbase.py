efb7707e87d0e95fd9ce80d474c8b7aba6d8513a
Quentin Peter <qaep2@cam.ac.uk>
2019-12-05 16:17:29 +0000

    Use PY3

--- ipykernel/kernelbase.py
+++ ipykernel/kernelbase.py
@@ -31,7 +31,7 @@ from zmq.eventloop.zmqstream import ZMQStream
 from traitlets.config.configurable import SingletonConfigurable
 from IPython.core.error import StdinNotImplementedError
 from ipython_genutils import py3compat
-from ipython_genutils.py3compat import unicode_type, string_types
+from ipython_genutils.py3compat import unicode_type, string_types, PY3
 from ipykernel.jsonutil import json_clean
 from traitlets import (
     Any, Instance, Float, Dict, List, Set, Integer, Unicode, Bool,
@@ -920,10 +920,11 @@ class Kernel(SingletonConfigurable):
         """Do one step of the input request loop."""
         # Allow matplotlib figures using GUI frameworks (e.g. qt, wx, gtk, tk)
         # to update
-        if sys.version_info >= (3, 4):
+        if PY3:
             is_main_thread = (threading.current_thread() is
                               threading.main_thread())
         else:
+            # Python 2
             is_main_thread = isinstance(threading.current_thread(),
                                         threading._MainThread)
         if (is_main_thread and 'matplotlib.pyplot' in sys.modules and

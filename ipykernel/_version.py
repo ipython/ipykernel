8185a170ba217aa8a385d8782afc7df2d26f712b
Steven Silvester <ssilvester@apple.com>
2021-08-12 06:47:20 -0500

    Release 6.1.0

--- ipykernel/_version.py
+++ ipykernel/_version.py
@@ -1,4 +1,4 @@
-version_info = (6, 0, 3)
+version_info = (6, 1, 0)
 __version__ = ".".join(map(str, version_info[:3]))
 
 # pep440 is annoying, beta/alpha/rc should _not_ have dots or pip/setuptools

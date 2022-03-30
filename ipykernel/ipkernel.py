881b6c8a232b025f019ee7d5a38fa9b18651801d
Bago Amirbekian <bago (at) databricks.com>
2022-03-29 17:00:16 -0700

    Include signature in completions.

--- ipykernel/ipkernel.py
+++ ipykernel/ipkernel.py
@@ -452,6 +452,7 @@ class IPythonKernel(KernelBase):
                         end=comp.end,
                         text=comp.text,
                         type=comp.type,
+                        signature=comp.signature,
                     )
                 )
 

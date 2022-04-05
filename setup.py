d0008b9cba45a6d9ebb78ef54bdb674ee4aa7c2c
Tomáš Linhart <pasmen@gmail.com>
2022-04-05 14:42:10 +0200

    Update setup.py
    Add project URLs to the `setup.py`. Especially adding the source URL is beneficial since it allows tools like Renovate (https://github.com/renovatebot/renovate) handle the dependencies with features like grouping the packages based on source URLs or URL prefixes (see https://docs.renovatebot.com/configuration-options/#matchsourceurlprefixes).

--- setup.py
+++ setup.py
@@ -60,6 +60,12 @@ setup_args = dict(
     long_description=LONG_DESCRIPTION,
     platforms="Linux, Mac OS X, Windows",
     keywords=["Interactive", "Interpreter", "Shell", "Web"],
+    project_urls={
+        "Documentation": "https://ipython.readthedocs.io/",
+        "Funding": "https://numfocus.org/",
+        "Source": "https://github.com/ipython/ipykernel",
+        "Tracker": "https://github.com/ipython/ipykernel/issues",
+    }
     python_requires=">=3.7",
     install_requires=[
         "debugpy>=1.0",

d08c94a95e2cffa64e76f31b7b8e5b5d687aa7d7
Eric Charles <eric@datalayer.io>
2022-05-24 06:30:08 +0200

    remove unneeded imports

--- ipykernel/control.py
+++ ipykernel/control.py
@@ -10,7 +10,6 @@ else:
 
 
 class ControlThread(Thread):
-
     def __init__(self, **kwargs):
         Thread.__init__(self, name="Control", **kwargs)
         self.io_loop = IOLoop(make_current=False)

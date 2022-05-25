64d63a0c47467278d07de9e27a5abd8cf4cd914e
Steven Silvester <steven.silvester@ieee.org>
2022-05-25 14:24:34 -0500

    force debugger:true in built wheel

--- hatch_build.py
+++ hatch_build.py
@@ -11,15 +11,21 @@ class CustomHook(BuildHookInterface):
         sys.path.insert(0, here)
         from ipykernel.kernelspec import make_ipkernel_cmd, write_kernel_spec
 
+        overrides = {}
+
         # When building a standard wheel, the executable specified in the kernelspec is simply 'python'.
         if version == "standard":
+            overrides["metadata"] = dict(debugger=True)
             argv = make_ipkernel_cmd(executable="python")
 
         # When installing an editable wheel, the full `sys.executable` can be used.
         else:
             argv = make_ipkernel_cmd()
 
+        overrides["argv"] = argv
+
         dest = os.path.join(here, "data_kernelspec")
         if os.path.exists(dest):
             shutil.rmtree(dest)
-        write_kernel_spec(dest, overrides={"argv": argv})
+
+        write_kernel_spec(dest, overrides=overrides)

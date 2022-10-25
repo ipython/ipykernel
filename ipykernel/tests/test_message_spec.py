99706182995e0fd5431965d4c9d96a8ce7afae12
Steven Silvester <steven.silvester@ieee.org>
2022-10-25 09:07:24 -0500

    Fix failing test and update matrix (#1010)

--- ipykernel/tests/test_message_spec.py
+++ ipykernel/tests/test_message_spec.py
@@ -509,9 +509,7 @@ def test_connect_request():
     flush_channels()
     msg = KC.session.msg("connect_request")
     KC.shell_channel.send(msg)
-    return msg["header"]["msg_id"]
-
-    msg_id = KC.kernel_info()
+    msg_id = msg["header"]["msg_id"]
     reply = get_reply(KC, msg_id, TIMEOUT)
     validate_message(reply, "connect_reply", msg_id)
 

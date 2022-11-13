bc7c9eea06218d25da2a6ac3e43c09759176d987
Steven Silvester <steven.silvester@ieee.org>
2022-11-13 06:44:39 -0600

    increase coverage

--- ipykernel/tests/test_eventloop.py
+++ ipykernel/tests/test_eventloop.py
@@ -1,7 +1,13 @@
 """Test eventloop integration"""
 
+import asyncio
+import threading
+import time
+
 import pytest
 import tornado
+
+from ipykernel.eventloops import enable_gui, loop_asyncio, loop_tk
 
 from .utils import execute, flush_channels, start_new_kernel
 
@@ -41,3 +47,27 @@ def test_asyncio_interrupt():
     flush_channels(KC)
     msg_id, content = execute(async_code, KC)
     assert content["status"] == "ok"
+
+
+def test_tk_loop(kernel):
+    def do_thing():
+        time.sleep(1)
+        kernel.app_wrapper.app.quit()
+
+    t = threading.Thread(target=do_thing)
+    t.start()
+    loop_tk(kernel)
+    t.join()
+
+
+def test_asyncio_loop(kernel):
+    def do_thing():
+        loop.call_soon(loop.stop)
+
+    loop = asyncio.get_event_loop()
+    loop.call_soon(do_thing)
+    loop_asyncio(kernel)
+
+
+def test_enable_gui(kernel):
+    enable_gui("tk", kernel)

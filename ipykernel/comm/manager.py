72e785394a859eb030461c3d6b3c1acf3b745daa
Steven Silvester <steven.silvester@ieee.org>
2022-12-07 07:59:00 -0600

    Revert "Fix Comm interface for downstream users (#1042)"
    This reverts commit c0f5b7e3a5287c288eff477ae70848decf25332d.

--- ipykernel/comm/manager.py
+++ ipykernel/comm/manager.py
@@ -5,16 +5,10 @@
 
 
 import comm.base_comm
-import traitlets
 import traitlets.config
 
 
 class CommManager(traitlets.config.LoggingConfigurable, comm.base_comm.CommManager):
-
-    kernel = traitlets.Instance("ipykernel.kernelbase.Kernel")
-    comms = traitlets.Dict()
-    targets = traitlets.Dict()
-
     def __init__(self, **kwargs):
         # CommManager doesn't take arguments, so we explicitly forward arguments
         traitlets.config.LoggingConfigurable.__init__(self, **kwargs)

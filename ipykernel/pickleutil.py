42a45f3eaf9bf6fac8d64f685e70da3f15b0e141
Steven Silvester <steven.silvester@ieee.org>
2023-03-07 10:44:25 -0600

    lint

--- ipykernel/pickleutil.py
+++ ipykernel/pickleutil.py
@@ -77,7 +77,7 @@ def use_dill():
     # dill doesn't work with cPickle,
     # tell the two relevant modules to use plain pickle
 
-    global pickle  # noq
+    global pickle  # noqa
     pickle = dill
 
     try:

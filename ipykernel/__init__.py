5875756e24dd65b0a1a9b79bd372feb418ef74e8
Steven Silvester <steven.silvester@ieee.org>
2024-02-10 10:27:02 -0600

    Unpin pytest-asyncio and update ruff config

--- ipykernel/__init__.py
+++ ipykernel/__init__.py
@@ -1,7 +1,1 @@
-from ._version import (
-    __version__,
-    kernel_protocol_version,
-    kernel_protocol_version_info,
-    version_info,
-)
-from .connect import *
+from .connect import *  # noqa: F403

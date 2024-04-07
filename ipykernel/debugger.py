689e54e8ca446af896156be467c5b63a2b6186f5
ryan-wan <ryan.wan@databricks.com>
2024-04-06 23:46:50 -0700

    update log

--- ipykernel/debugger.py
+++ ipykernel/debugger.py
@@ -285,7 +285,7 @@ class DebugpyClient:
     def connect_tcp_socket(self):
         """Connect to the tcp socket."""
         self.debugpy_socket.connect(self._get_endpoint())
-        print("DEBUGPYCLIENT - connected to " + self.debugpy_socket.getsockopt(ROUTING_ID).decode())
+        print("DEBUGPYCLIENT - deubgppy socket connected to " + self.debugpy_socket.getsockopt(zmq.LAST_ENDPOINT))
         self.routing_id = self.debugpy_socket.getsockopt(ROUTING_ID)
 
     def disconnect_tcp_socket(self):
@@ -448,7 +448,7 @@ class Debugger:
             print("debugpy: after sending request")
             print(
                 "DEBUGPYCLIENT - send message to shell channel "
-                + self.shell_socket.getsockopt(ROUTING_ID).decode()
+                + self.shell_socket.getsockopt(zmq.LAST_ENDPOINT)
             )
 
             msg = await self.shell_socket.recv_multipart()

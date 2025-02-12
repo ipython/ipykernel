f87591205ed8e935f3ad91a372df97165535a577
M Bussonnier <bussonniermatthias@gmail.com>
2025-02-12 11:38:24 +0100

    Stop support for Python 3.9
    New major version are a good time to drop support for older versions.
    I thihnk 3.9 should not be an issue, I woudl argue for removal of 3.10
    as well.

--- ipykernel/ipkernel.py
+++ ipykernel/ipkernel.py
@@ -160,10 +160,7 @@ class IPythonKernel(KernelBase):
         self._new_threads_parent_header = {}
         self._initialize_thread_hooks()
 
-        if hasattr(gc, "callbacks"):
-            # while `gc.callbacks` exists since Python 3.3, pypy does not
-            # implement it even as of 3.9.
-            gc.callbacks.append(self._clean_thread_parent_frames)
+        gc.callbacks.append(self._clean_thread_parent_frames)
 
     help_links = List(
         [

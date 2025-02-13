cefa3e9f9f1645bf132c64d770fb521d8614821e
David Brochart <david.brochart@gmail.com>
2025-02-13 13:11:34 +0100

    Fix test_start_kernel

--- tests/test_start_kernel.py
+++ tests/test_start_kernel.py
@@ -24,7 +24,10 @@ def test_ipython_start_kernel_userns():
 
     with setup_kernel(cmd) as client:
         client.inspect("custom")
-        msg = client.get_shell_msg(timeout=TIMEOUT)
+        while True:
+            msg = client.get_shell_msg(timeout=TIMEOUT)
+            if msg["msg_type"] == "inspect_reply":
+                break
         content = msg["content"]
         assert content["found"]
         text = content["data"]["text/plain"]
@@ -36,7 +39,10 @@ def test_ipython_start_kernel_userns():
         content = msg["content"]
         assert content["status"] == "ok"
         client.inspect("usermod")
-        msg = client.get_shell_msg(timeout=TIMEOUT)
+        while True:
+            msg = client.get_shell_msg(timeout=TIMEOUT)
+            if msg["msg_type"] == "inspect_reply":
+                break
         content = msg["content"]
         assert content["found"]
         text = content["data"]["text/plain"]
@@ -60,7 +66,10 @@ def test_ipython_start_kernel_no_userns():
         content = msg["content"]
         assert content["status"] == "ok"
         client.inspect("usermod")
-        msg = client.get_shell_msg(timeout=TIMEOUT)
+        while True:
+            msg = client.get_shell_msg(timeout=TIMEOUT)
+            if msg["msg_type"] == "inspect_reply":
+                break
         content = msg["content"]
         assert content["found"]
         text = content["data"]["text/plain"]

4c6d56c0d6b0c6f6dcb5436f535615ab591a8122
Min RK <benjaminrk@gmail.com>
2025-10-20 12:11:35 -0700

    test that matplotlib event loop integration is responsive

---
+++ tests/test_matplotlib_eventloops.py
@@ -0,0 +1,89 @@
+import sys
+import time
+
+import pytest
+from jupyter_client.blocking.client import BlockingKernelClient
+
+from .test_eventloop import qt_guis_avail
+from .utils import assemble_output
+
+guis = []
+if not sys.platform.startswith("tk"):
+    guis.append("tk")
+if qt_guis_avail:
+    guis.append("qt")
+if sys.platform == "darwin":
+    guis.append("osx")
+
+backends = {
+    "tk": "tkagg",
+    "qt": "qtagg",
+    "osx": "macosx",
+}
+
+
+def execute(
+    kc: BlockingKernelClient,
+    code: str,
+    timeout=10,
+):
+    msg_id = kc.execute(code)
+    stdout, stderr = assemble_output(kc.get_iopub_msg, timeout=timeout, parent_msg_id=msg_id)
+    assert not stderr.strip()
+    return stdout.strip(), stderr.strip()
+
+
+@pytest.mark.parametrize("gui", guis)
+def test_matplotlib_gui(kc, gui):
+    """Make sure matplotlib activates and its eventloop runs while the kernel is also responsive"""
+    pytest.importorskip("matplotlib", reason="this test requires matplotlib")
+    stdout, stderr = execute(kc, f"%matplotlib {gui}")
+    assert not stderr
+    execute(
+        kc,
+        """
+    from concurrent.futures import Future
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+    """,
+    )
+    stdout, _ = execute(kc, "print(mpl.get_backend())")
+    assert stdout == backends[gui]
+    execute(
+        kc,
+        """
+fig, ax = plt.subplots()
+timer = fig.canvas.new_timer(interval=10)
+f = Future()
+
+call_count = 0
+def add_call():
+    global call_count
+    call_count += 1
+    if not f.done():
+        f.set_result(None)
+
+timer.add_callback(add_call)
+timer.start()
+""",
+    )
+    # wait for the first call (up to 10 seconds)
+    for _ in range(100):
+        stdout, _ = execute(kc, "print(f.done())")
+        if stdout.strip() == "True":
+            break
+        if stdout == "False":
+            time.sleep(0.1)
+        else:
+            pytest.fail(f"Unexpected output {stdout}")
+    time.sleep(0.25)
+    stdout, _ = execute(kc, "print(call_count)")
+    call_count = int(stdout)
+    assert call_count > 0
+    time.sleep(0.25)
+    stdout, _ = execute(kc, "timer.stop()\nprint(call_count)")
+    call_count_2 = int(stdout)
+    assert call_count_2 > call_count
+    stdout, _ = execute(kc, "print(call_count)")
+    call_count_3 = int(stdout)
+    assert call_count_3 <= call_count_2 + 5

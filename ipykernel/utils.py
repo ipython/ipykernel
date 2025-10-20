d805bd71cc2a6b6742d2ab1bc5a505225a51dfaf
Min RK <benjaminrk@gmail.com>
2025-10-20 11:14:52 -0700

    fix ContextVar persistence across cells
    backports asyncio.create_task(context=...) behavior from 3.11

--- ipykernel/utils.py
+++ ipykernel/utils.py
@@ -1,7 +1,17 @@
 """Utilities"""
 
+from __future__ import annotations
+
+import asyncio
+import sys
 import typing as t
 from collections.abc import Mapping
+from contextvars import copy_context
+from functools import partial, wraps
+
+if t.TYPE_CHECKING:
+    from collections.abc import Callable
+    from contextvars import Context
 
 
 class LazyDict(Mapping[str, t.Any]):
@@ -24,3 +34,41 @@ class LazyDict(Mapping[str, t.Any]):
 
     def __iter__(self):
         return iter(self._dict)
+
+
+def _async_in_context(f: Callable, context: Context | None = None) -> Callable:
+    """
+    Wrapper to run a coroutine in a persistent ContextVar Context.
+
+    Backports asyncio.create_task(context=...) behavior from Python 3.11
+    """
+    if context is None:
+        context = copy_context()
+
+    if sys.version_info >= (3, 11):
+
+        @wraps(f)
+        async def run_in_context(*args, **kwargs):
+            coro = f(*args, **kwargs)
+            return await asyncio.create_task(coro, context=context)
+
+        return run_in_context
+
+    # don't need this backport when we require 3.11
+    # context_holder so we have a modifiable container for later calls
+    context_holder = [context]
+
+    async def preserve_context(f, *args, **kwargs):
+        """call a coroutine, preserving the context after it is called"""
+        try:
+            return await f(*args, **kwargs)
+        finally:
+            # persist changes to the context for future calls
+            context_holder[0] = copy_context()
+
+    @wraps(f)
+    async def run_in_context(*args, **kwargs):
+        ctx = context_holder[0]
+        return await ctx.run(partial(asyncio.create_task, preserve_context(f, *args, **kwargs)))
+
+    return run_in_context

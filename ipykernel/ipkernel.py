43197e786f3189b87d8f30d4a017b20ec4f4855d
Ian Thomas <ianthomas23@gmail.com>
2025-11-26 09:30:54 +0000

    Iterate

--- ipykernel/ipkernel.py
+++ ipykernel/ipkernel.py
@@ -292,7 +292,7 @@ class IPythonKernel(KernelBase):
         builtins.input = self.raw_input
 
         self._save_getpass = getpass.getpass
-        getpass.getpass = self.getpass  # type:ignore[assignment]
+        getpass.getpass = self.getpass
 
     def _restore_input(self):
         """Restore raw_input, getpass"""
